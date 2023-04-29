92c1ae67069ac6ce77b9de40809aa568eec24de5
youredith <youredith3@gmail.com>
2023-04-30 01:05:54 +0900

    refactor: use toLocaleString, ternary operator

--- pages/billionaire/[id].js
+++ pages/billionaire/[id].js
@@ -23,109 +23,112 @@ export default function BillionairePage() {
     }
   }, [id]);
 
-  if (!billionaire) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div className="container">
-      <div className="card">
-        <div className="photo">
-          <img src={billionaire.squareImage} />
-        </div>
-        <div className="details">
-          <span className="fullName">{billionaire.name}</span>
-          <span className="networth">
-            Networth : {formatAsset(billionaire.netWorth)}
-          </span>
+    <>
+      {!billionaire ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="container">
+          <div className="card">
+            <div className="photo">
+              <img src={billionaire.squareImage} />
+            </div>
+            <div className="details">
+              <span className="fullName">{billionaire.name}</span>
+              <span className="networth">
+                Networth : {formatAsset(billionaire.netWorth)}
+              </span>
 
-          <span className="country">Country : {billionaire.country}</span>
+              <span className="country">Country : {billionaire.country}</span>
 
-          <span className="industry">Industry : {billionaire.industries}</span>
+              <span className="industry">
+                Industry : {billionaire.industries}
+              </span>
 
-          <span className="bio">{billionaire.bio}</span>
-        </div>
-        <div className="financial-assets">
-          <h1>Financial Assets</h1>
-          <div className="tickerCards">
-            {billionaire.financialAssets.map((asset, id) => {
-              return (
-                <div className="assetCard" key={id}>
-                  <span className="ticker">Ticker : {asset.ticker}</span>
-                  <span className="shares">
-                    Shares : {formatWithCommas(asset.numberOfShares)}
-                  </span>
-                  <span className="excersiePrice">
-                    Excersie Price : ${asset.sharePrice}
-                  </span>
-                </div>
-              );
-            })}
+              <span className="bio">{billionaire.bio}</span>
+            </div>
+            <div className="financial-assets">
+              <h1>Financial Assets</h1>
+              <div className="tickerCards">
+                {billionaire.financialAssets.map((asset, id) => {
+                  return (
+                    <div className="assetCard" key={id}>
+                      <span className="ticker">Ticker : {asset.ticker}</span>
+                      <span className="shares">
+                        Shares : {formatWithCommas(asset.numberOfShares)}
+                      </span>
+                      <span className="excersiePrice">
+                        Excersie Price : ${asset.sharePrice}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
 
-      <style jsx>{`
-        .container {
-          width: 100vw;
-          margin-top: 50px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-        .card {
-          padding: 50px;
-          width: 70%;
-          background-color: #ffffff;
-          border-radius: 10px;
-        }
-        .details {
-          margin-top: 20px;
-          display: flex;
-          flex-direction: column;
-          gap: 1rem;
-        }
-        .photo img {
-          height: auto;
-          border-radius: 10px;
-        }
-        .fullName {
-          font-size: 1.5rem;
-          font-weight: bold;
-        }
-        .networth,
-        .country,
-        .industry,
-        .bio {
-          font-size: 1.2rem;
-        }
-        .financial-assets {
-          margin-top: 50px;
-        }
-        h1 {
-          margin-bottom: 20px;
-        }
-        .tickerCards {
-          display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-          gap: 10px;
-        }
-        .assetCard {
-          display: flex;
-          flex-direction: column;
-          gap: 10px;
-          background-color: #f5f5f5;
-          padding: 20px;
-          border-radius: 10px;
-        }
-        .ticker,
-        .shares,
-        .excersiePrice {
-          font-weight: bold;
-          font-size: 1.1rem;
-        }
-      `}</style>
-    </div>
+          <style jsx>{`
+            .container {
+              width: 100vw;
+              margin-top: 50px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+            .card {
+              padding: 50px;
+              width: 70%;
+              background-color: #ffffff;
+              border-radius: 10px;
+            }
+            .details {
+              margin-top: 20px;
+              display: flex;
+              flex-direction: column;
+              gap: 1rem;
+            }
+            .photo img {
+              height: auto;
+              border-radius: 10px;
+            }
+            .fullName {
+              font-size: 1.5rem;
+              font-weight: bold;
+            }
+            .networth,
+            .country,
+            .industry,
+            .bio {
+              font-size: 1.2rem;
+            }
+            .financial-assets {
+              margin-top: 50px;
+            }
+            h1 {
+              margin-bottom: 20px;
+            }
+            .tickerCards {
+              display: grid;
+              grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+              gap: 10px;
+            }
+            .assetCard {
+              display: flex;
+              flex-direction: column;
+              gap: 10px;
+              background-color: #f5f5f5;
+              padding: 20px;
+              border-radius: 10px;
+            }
+            .ticker,
+            .shares,
+            .excersiePrice {
+              font-weight: bold;
+              font-size: 1.1rem;
+            }
+          `}</style>
+        </div>
+      )}
+    </>
   );
 }
-``;
--- utils/formatNumbers.js
+++ utils/formatNumbers.js
@@ -7,16 +7,6 @@ export function formatAsset(value) {
 }
 
 export function formatWithCommas(value) {
-  const valueAsString = value.toString();
-  let reversedValue = valueAsString.split("").reverse();
-  let stringWithCommas = "";
-
-  for (let i = 0; i < reversedValue.length; i++) {
-    stringWithCommas += reversedValue[i];
-    if ((i + 1) % 3 === 0 && i !== reversedValue.length - 1) {
-      stringWithCommas += ",";
-    }
-  }
-
-  return stringWithCommas.split("").reverse().join("");
+  return value.toLocaleString('en-US');
 }
+
